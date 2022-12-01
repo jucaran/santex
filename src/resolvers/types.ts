59c7275ab0d3a8bec5da74756a8bda4e9a66ebd0
Juanque <juan.castro.arancibia@gmail.com>
2022-12-01 16:12:50 -0300

    Adds api response types

---
+++ src/resolvers/types.ts
@@ -0,0 +1,79 @@
+interface ApiArea {
+  id: string
+  name: string
+  code: string
+  flag: string
+}
+
+interface ApiSeason {
+  id: number
+  startDate: string
+  endDate: string
+  currentMatchday: number
+  winner?: string
+}
+
+interface ApiCompetition {
+  id: number
+  name: string
+  code: string
+  type: string
+  emblem: string
+}
+
+interface ApiCoach {
+  id: number
+  firstName?: string
+  lastName?: string
+  name?: string
+  dateOfBirth?: string
+  nationality?: string
+  contract: { start?: string; until?: string }
+}
+
+interface ApiPlayer {
+  id: number
+  name?: string
+  position?: string
+  dateOfBirth?: string
+  nationality?: string
+}
+
+interface ApiTeam {
+  area: ApiArea
+  id: number
+  name: string
+  shortName: string
+  tla: string
+  crest: string
+  address: string
+  website: string
+  founded: number
+  clubColors: string
+  venue: string
+  runningCompetitions: ApiCompetition[]
+  coach: ApiCoach
+  squad: ApiPlayer[]
+  staff: []
+  lastUpdated: string
+}
+
+export interface ApiCompetitionResponse {
+  area: ApiArea
+  id: number
+  name: string
+  code: string
+  type: string
+  emblem: string
+  currentSeason: ApiSeason
+  seasons: ApiSeason[]
+  lastUpdated: string
+}
+
+export interface ApiCompetitionTeamsResponse {
+  count: number
+  filters: { season: string }
+  competition: ApiCompetition
+  season: ApiSeason
+  teams: ApiTeam[]
+}
